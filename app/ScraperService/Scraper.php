e33e40bdc627883b9f9499322341abbf48354b74
toufik saadane <toufiksaadane@mbp-van-toufik.home>
2020-09-06 21:02:45 +0200

    adding nederland en marrokko

--- app/ScraperService/Scraper.php
+++ app/ScraperService/Scraper.php
@@ -13,7 +13,7 @@ class Scraper
 
 
     /**
-     * @return false|string
+     * @return array
      */
     public function totalOverview()
     {
@@ -35,15 +35,66 @@ class Scraper
         );
 
         $ar = array_combine($this->key, $this->value);
-        return json_encode(["overview" => $ar]);
+        return ["overview" => $ar];
     }
 
+    public function scrapingMorocco(){
 
+        $c = new Client();
+        $data = $c->request("GET", "https://www.worldometers.info/coronavirus/country/morocco/");
+
+        $k = $data->filter("#maincounter-wrap  > h1")->each(
+            function ($item) {
+
+                $this->key[] = str_replace(":", " ", $item->text());
+
+            }
+        );
+
+        $v = $data->filter(".maincounter-number  > span")->each(
+            function ($item) {
+                $this->value[] = $item->text();
+            }
+        );
+
+        $ar = array_combine($this->key, $this->value);
+        return ["morocco" => $ar];
+    }
+
+    public function scrapingNederland(){
+
+        $c = new Client();
+        $data = $c->request("GET", "https://www.worldometers.info/coronavirus/country/netherlands/");
+
+        $k = $data->filter("#maincounter-wrap  > h1")->each(
+            function ($item) {
+
+                $this->key[] = str_replace(":", " ", $item->text());
+
+            }
+        );
+
+        $v = $data->filter(".maincounter-number  > span")->each(
+            function ($item) {
+                $this->value[] = $item->text();
+            }
+        );
+
+        $ar = array_combine($this->key, $this->value);
+        return ["nederland" => $ar];
+    }
+    public function data(){
+        return collect([
+            $this->totalOverview(),
+            $this->scrapingMorocco(),
+            $this->scrapingNederland()
+        ])->toJson();
+    }
     public function createJsonFile(){
         $file = date('Y-m-d'). '_file.json';
         $destinationPath=public_path()."/ScraperStorage/json/";
         if (!is_dir($destinationPath)) {  mkdir($destinationPath,0777,true);  }
-        File::put($destinationPath.$file,$this->totalOverview());
+        File::put($destinationPath.$file,$this->data());
         echo "done";
         return;
     }
